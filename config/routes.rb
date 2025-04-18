47af414187bed689843125472a03bc323913d898
imada1290 <imada.yuya@laiblitz.co.jp>
2025-04-18 18:01:16 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "rooms#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
 end
