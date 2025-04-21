fac9e107a591ca3d5d9099c12fe41093b77d0f02
imada1290 <imada.yuya@laiblitz.co.jp>
2025-04-21 15:35:46 +0900

    チャットルームの削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "rooms#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create] do
+  resources :rooms, only: [:new, :create, :destroy] do
     resources :messages, only: [:index, :create]
   end
 end
